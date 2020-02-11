6ef4105bbe5348de1e3fd28a6fb0d47edec76efe
Sara Laffin <sarala@umich.edu>
2020-02-11 11:50:08 -0500

    prevent piece from going into peg div when smaller piece is already there

--- script.js
+++ script.js
@@ -14,10 +14,15 @@ moveButtons.forEach(button => button.addEventListener("click", movePiece))
 function movePiece(eve) {
     let piecesPresent = eve.path[1].querySelectorAll(".piece")
     console.log("how many pieces already here: ",piecesPresent.length)
-    eve.path[1].insertBefore(activePiece, eve.path[1].childNodes[0])
     let pieceSizes = []
     piecesPresent.forEach(piece => {
         pieceSizes.push(parseInt(piece.dataset.size))
     })
-    console.log("the size of pieces already here is: ",pieceSizes)
+    console.log("the smallest piece already here is: ",pieceSizes)
+    console.log("the smallest piece already here is: ",pieceSizes.sort(function(a, b){return a - b}))
+    if (piecesPresent.length == 0) {
+        eve.path[1].insertBefore(activePiece, eve.path[1].childNodes[0])
+    } else if (parseInt(activePiece.dataset.size) < pieceSizes[0]) {
+        eve.path[1].insertBefore(activePiece, eve.path[1].childNodes[0])
+    }
 }
