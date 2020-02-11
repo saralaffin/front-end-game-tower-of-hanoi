c40bbd8a262e61092553a51c1ba840502c40d5b0
Sara Laffin <sarala@umich.edu>
2020-02-11 10:28:22 -0500

    Click disk and move to another div

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let disks = document.querySelectorAll(".disk")
 disks.forEach(disk => disk.addEventListener("click", changeColor))
-
+let activeDisk = null
 function changeColor(eve) {
     eve.target.style.backgroundColor = "darkgreen"
+    activeDisk = eve.target
+}
+
+let moveButtons = document.querySelectorAll("button")
+moveButtons.forEach(button => button.addEventListener("click", moveDisk))
+
+function moveDisk(eve) {
+    eve.target.parentNode.appendChild(activeDisk)
 }
