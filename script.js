2e5b9e75680b5291f4c8f308dd163ccaabb942a3
Sara Laffin <sarala@umich.edu>
2020-02-11 12:06:52 -0500

    add checkForWin condition and console log when game is won

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let activePiece = null
 function changeColor(eve) {
     eve.target.style.backgroundColor = "darkgreen"
     activePiece = eve.target
-    console.log("The size of this piece is: ",parseInt(activePiece.dataset.size))
 }
 
 let moveButtons = document.querySelectorAll("button")
@@ -13,16 +12,29 @@ moveButtons.forEach(button => button.addEventListener("click", movePiece))
 
 function movePiece(eve) {
     let piecesPresent = eve.path[1].querySelectorAll(".piece")
-    console.log("how many pieces already here: ",piecesPresent.length)
+    //add all piece sizes to one array
     let pieceSizes = []
     piecesPresent.forEach(piece => {
         pieceSizes.push(parseInt(piece.dataset.size))
     })
-    console.log("the smallest piece already here is: ",pieceSizes)
-    console.log("the smallest piece already here is: ",pieceSizes.sort(function(a, b){return a - b}))
-    if (piecesPresent.length == 0) {
+
+    //order pieces from smallest to largest
+    pieceSizes.sort(function(a, b){return a - b})
+
+    if (piecesPresent.length == 0) { //if peg is empty, allow new piece to enter
         eve.path[1].insertBefore(activePiece, eve.path[1].childNodes[0])
-    } else if (parseInt(activePiece.dataset.size) < pieceSizes[0]) {
+    } else if (parseInt(activePiece.dataset.size) < pieceSizes[0]) { //add piece if smaller than the smallest piece
         eve.path[1].insertBefore(activePiece, eve.path[1].childNodes[0])
+    }
+
+    //check for a winning condition
+    checkForWin()
+}
+
+function checkForWin() {
+    let lastPeg = document.querySelector(".lastPeg")
+    let numberInLast = lastPeg.querySelectorAll(".piece").length
+    if (numberInLast == 3) {
+        console.log("You've won!")
     }
 }
