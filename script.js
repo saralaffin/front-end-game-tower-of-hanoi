7ddd6e56a9870a54f3729180a36c6ca244ce8729
Sara Laffin <sarala@umich.edu>
2020-02-11 10:59:48 -0500

    prevent disk from going into already occupied div

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ moveButtons.forEach(button => button.addEventListener("click", moveDisk))
 function moveDisk(eve) {
     let disksPresent = eve.path[1].querySelectorAll(".disk")
     console.log("how many disks already here: ",disksPresent.length)
+    if (disksPresent.length == 0) {
+        eve.target.parentNode.appendChild(activeDisk)
+    }
     disksPresent.forEach(disk => console.log("the size of box already here is: ",parseInt(disk.dataset.size)))
-    eve.target.parentNode.appendChild(activeDisk)
 }
